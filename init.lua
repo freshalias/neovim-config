b01a48b448bf23e09f57b76a5e2a73ab7fba846c
freshalias <108597418+freshalias@users.noreply.github.com>
2023-03-14 19:44:38 +0800

    change the order in init.lua to init packer first

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
---options
-require("core.options")
-require("core.keymaps")
 -- packer
 require("packer.packer-setup")
 require("packer.lualine")
 require("packer.tree")
 require("packer.lsp")
 require("packer.cmp")
+--options
+require("core.options")
+require("core.keymaps")
 -- to solve vimL question
